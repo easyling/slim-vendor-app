976145be2d1f6ecb527a347098dd348a46941a2a
Daniel Varga <dvarga@skawa.hu>
2016-06-10 12:28:59 +0200

    test endpoint changed to 8082 port

--- test/test_config.dart
+++ test/test_config.dart
@@ -1,2 +1,2 @@
 final Uri easylingEndpoint = Uri.parse('http://localhost:8888');
-final Uri vendorEndpoint = Uri.parse('http://localhost:8080');
+final Uri vendorEndpoint = Uri.parse('http://localhost:8082');
